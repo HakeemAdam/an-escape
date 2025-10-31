e636d08a9a246e69c59e6b1b3447220fdada25f3
Hakkem Adam <tuffassassin11@gmail.com>
2025-10-31 15:35:49 +0100

    ui added and connected

--- sounding/testPD.gd
+++ sounding/testPD.gd
@@ -3,6 +3,10 @@ extends Node2D
 var player : AudioStreamPlayer
 var pd: AudioStreamPlaybackPD
 
+@onready var btn := $Ui/VBoxContainer/Button
+@onready var bpmSlider := $Ui/VBoxContainer/bpmSlider
+@onready var bpmValLabel := $Ui/VBoxContainer/bpmValueLabel
+
 func _ready() -> void:
 	
 	player = AudioStreamPlayer.new()
@@ -15,5 +19,16 @@ func _ready() -> void:
 	pd = player.get_stream_playback()
 	
 	pd.open_patch("E:/dirs2/gdprojects/an-escape/sounding/pds/example.pd")
+	btn.pressed.connect(onStartPreesed)
+	bpmSlider.value_changed.connect(onBpmChnaged)
 	
-	pd.send_bang("bang")
+	pd.send_float("bpm", bpmSlider.value)
+	bpmValLabel.text = str(bpmSlider.value)
+	#pd.send_bang("start")
+
+func onStartPreesed():
+	pd.send_bang("start")
+
+func onBpmChnaged(value: float):
+		pd.send_float("bpm", value)
+		bpmValLabel.text = str(value)
