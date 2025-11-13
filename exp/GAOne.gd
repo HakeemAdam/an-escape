7b966071fdd08d35edb00f9c0d86e45a938cee46
Hakkem Adam <tuffassassin11@gmail.com>
2025-11-13 19:58:00 +0100

    random fitness selection

--- exp/GAOne.gd
+++ exp/GAOne.gd
@@ -3,34 +3,56 @@ extends Node2D
 
 var screenSize: Vector2
 var totalPop: int = 100
-var element: Array[Ind]=[]
+var elements: Array[Ind]=[]
+var matingPool: Array[Ind]=[]
 
 func _ready() -> void:
 	screenSize = get_viewport_rect().size
 	initPop()
+	SelectMatingPool()
+	print("mating pool Size:", matingPool.size())
+	
+	
 	pass 
 
 
 
 func _process(_delta: float) -> void:
+
 	queue_redraw()
 	pass
 
-func initPop():
+func initPop() -> void:
 	for i in totalPop:
 		var el = preload("res://exp/ind.gd").new()
 		
 		var pos = Vector2(randf_range(0.0, screenSize.x-el.IRadius),randf_range(0.0, screenSize.y-el.IRadius))
 		el.IPos=pos;
 		
-		var dna = randi_range(i,255)
+		var dna = randi_range(i,255) / 255.0
 		el.Idna = dna
 		print("DNA: ", dna)
 		
-		var col = dna / 255.0
+		var col = dna 
 		el.IColor = Color(col, col, col, 1.0)
-		print("Color: ", col)
 		
 		add_child(el)
-		element.push_back(el)
+		elements.push_back(el)
 		
+func SelectMatingPool() -> void:
+	matingPool.clear()
+	for i in elements:
+		var fit = evalFitness(i.Idna)
+		
+		var prob = fit / 3.0
+		if randf() < prob:
+			matingPool.push_back(i)
+	pass
+	
+func evalFitness(element: float) -> int:
+	if element > 0.15 :
+		return 3
+	elif element > 0.25:
+		return 2
+	else:
+		return 1
